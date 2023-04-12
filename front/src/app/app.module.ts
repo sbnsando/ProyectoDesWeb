c0d93cf069601519a8f6887b1a8826aa51f61286
Sebastian Sandoval <sebastian-sandoval@javeriana.edu.co>
2023-04-12 18:30:26 -0500

    Header

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule
